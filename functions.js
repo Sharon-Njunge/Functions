54e572ee85c1c5ba23cc62732c71e7717205f2c1
Sharon-Njung'e <njungesharonwanjiku@gmail.com>
2024-03-29 19:39:54 +0300

    add files

---
+++ functions.js
@@ -0,0 +1,26 @@
+function sorting(){
+    const countries = ["Burundi", "Kigali", "Kampala", "Nairobi"]
+   
+}
+console.log(countries.sort());
+console.log(countries.reverse());
+
+function checkElements(){
+    let arr1=arr1.forEach(element =>{
+        if(element>0){
+            console.log("positive");
+        }else if(element>0){
+            console.log("negative");
+        }else{
+            console.log("zero");
+        }
+    }
+         )
+}
+function multiply(nums){
+    nums.forEach((nums) => {
+        console.log(num*2);
+    }
+
+    )
+}
